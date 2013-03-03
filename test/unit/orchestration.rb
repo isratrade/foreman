2187f5590b3af9d29407ec70fd3bf798e076460d
Joseph Mitchell Magen <jmagen@redhat.com>
2013-03-03 17:41:12 +0200

    changed orcestration test to use to Host::Managed

--- test/unit/orchestration.rb
+++ test/unit/orchestration.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class OrchestrationTest < ActiveSupport::TestCase
   def test_host_should_have_queue
-    h = Host.new
+    h = Host::Managed.new
     assert_respond_to h, :queue
   end
 
   def test_action_queue_should_be_an_array
-    assert_kind_of Array, Host.get_external_actions
+    assert_kind_of Array, Host::Managed.get_external_actions
   end
 end
