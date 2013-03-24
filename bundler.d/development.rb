d6fd5ca79a1b6b5fe8bcae8001e8648a15935659
Joseph Mitchell Magen <jmagen@redhat.com>
2013-03-24 16:09:39 +0200

    added pry gems

--- bundler.d/development.rb
+++ bundler.d/development.rb
@@ -1,10 +1,11 @@
 group :development do
   # To use debugger
   gem "ruby-debug", :platforms => :ruby_18, :require => false
-  gem "ruby-debug19", :platforms => :ruby_19, :require => false
+  gem "debugger", :platforms => :ruby_19, :require => false
   gem 'maruku'
   gem 'single_test'
-  gem 'pry'
+  gem 'pry-rails'
+  gem 'pry-debugger'
   gem "term-ansicolor"
 #  gem 'rack-mini-profiler'
 
