5d4ad061e7d8fe56d58f0485abfd5b9730685fc0
Joseph Mitchell Magen <jmagen@redhat.com>
2013-03-28 09:53:13 +0200

    new host and role test

--- test/integration/js/role_test.rb
+++ test/integration/js/role_test.rb
@@ -10,4 +10,13 @@ class RoleTest < ActionDispatch::IntegrationTest
      assert_delete_row(roles_path, "Manager")
   end
 
+  test "check and uncheck all roles" do
+    visit roles_path
+    click_link "Manager"
+    click_link "uncheck_all_roles"
+    assert has_unchecked_field?("role_permissions_"), "Expected checkbox NOT to be checked, but it was"
+    click_link "check_all_roles"
+    assert has_checked_field?("role_permissions_"), "Expected checkbox to be checked, but it was NOT"
+  end
+
 end
