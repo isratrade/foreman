a7f3316f3a4e2547a67e730cff88e318728b84bf
Joseph Mitchell Magen <jmagen@redhat.com>
2013-03-31 10:00:43 +0300

    new jquery version

--- bundler.d/assets.rb
+++ bundler.d/assets.rb
@@ -2,9 +2,9 @@ group :assets do
   gem 'sass-rails', '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
-  gem "jquery-rails"
+  gem "jquery-rails", "2.0.3"
   gem 'jquery-ui-rails'
-  gem "therubyracer"
+  gem "therubyracer", "0.11.3"
   gem "less-rails"
   gem "twitter-bootstrap-rails"
 end
