2f90bf7a7cb39e371b4fb1ac3803d94b0522575b
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-04 11:05:09 +0300

    removed platform 19 restriction for minitest

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -1,6 +1,6 @@
 group :test do
   gem 'mocha', :require => false
-  gem 'minitest', '~> 3.5', :platforms => :ruby_19
+  gem 'minitest', '~> 3.5'
   gem 'single_test'
   gem 'shoulda', "3.0.1"
   gem 'rr'
