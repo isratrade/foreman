eb2214a27c856a1e93fc1c66cc965fae86fb9842
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-18 12:29:17 +0300

    modified paths notes on application.rb

--- config/application.rb
+++ config/application.rb
@@ -33,17 +33,21 @@ module Foreman
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/app/controllers/concerns"]
+    # will be default in Rails 4 models/concerns and controllers controllers
+    config.autoload_paths += %W(#{config.root}/app/controllers/concerns)
+    config.autoload_paths += %W(#{config.root}/app/models/concerns)
     config.autoload_paths += %W(#{config.root}/app/services)
     config.autoload_paths += %W(#{config.root}/app/observers)
+    # TODO I think app/mailers and responders is default after 3.2.3
     config.autoload_paths += %W(#{config.root}/app/mailers)
-    config.autoload_paths += %W(#{config.root}/app/models/concerns)
+    config.autoload_paths += %W(#{config.root}/app/responders)
     config.autoload_paths += %W(#{config.root}/app/models/auth_sources)
     config.autoload_paths += %W(#{config.root}/app/models/compute_resources)
     config.autoload_paths += %W(#{config.root}/app/models/operatingsystems)
     config.autoload_paths += %W(#{config.root}/app/models/parameters)
     config.autoload_paths += %W(#{config.root}/app/models/trends)
     config.autoload_paths += %W(#{config.root}/app/models/taxonomies)
+    # TODO - what is Dir[ ] vs. %W
 #   config.autoload_paths += Dir["#{config.root}/app/models/**/*"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
