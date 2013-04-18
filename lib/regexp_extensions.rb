bcaef9e90132082eb0789b8a6d345aa4af1a9f1b
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-18 12:17:55 +0300

    added if RUBY 1.8 to regexp_extensions

--- lib/regexp_extensions.rb
+++ lib/regexp_extensions.rb
@@ -3,12 +3,14 @@
 # ruby2ruby sets some constants, but not ONCE.  ruby_parser 2.x checks for
 # ONCE and then causes duplicate definitions of ENC_*.
 class Regexp
-  ONCE = 0 unless defined? ONCE # FIX: remove this - it makes no sense
+  if RUBY_VERSION =~ /^1\.8/
+    ONCE = 0 unless defined? ONCE # FIX: remove this - it makes no sense
 
-  unless defined? ENC_NONE then
-    ENC_NONE = /x/n.options
-    ENC_EUC  = /x/e.options
-    ENC_SJIS = /x/s.options
-    ENC_UTF8 = /x/u.options
+    unless defined? ENC_NONE then
+      ENC_NONE = /x/n.options
+      ENC_EUC  = /x/e.options
+      ENC_SJIS = /x/s.options
+      ENC_UTF8 = /x/u.options
+    end
   end
 end
