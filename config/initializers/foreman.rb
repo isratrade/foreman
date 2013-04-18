2463b7dc79d0d1309248d8ecdc697c2aa1a09b21
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-18 14:22:53 +0300

    paste lib/foreman.rb into intializers/foreman.rb

--- config/initializers/foreman.rb
+++ config/initializers/foreman.rb
@@ -1,4 +1,17 @@
-require 'foreman'
+require 'foreman/access_permissions'
+require 'foreman/default_data/loader'
+require 'foreman/default_settings/loader'
+require 'foreman/renderer'
+require 'foreman/controller'
+require 'net'
+require 'foreman/provision' if SETTINGS[:unattended]
+
+module Foreman
+  # generate a UUID
+  def self.uuid
+    UUIDTools::UUID.random_create.to_s
+  end
+end
 
 # We load the default settings if they are not already present
 Foreman::DefaultSettings::Loader.load
