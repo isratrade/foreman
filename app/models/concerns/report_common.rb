3d00bff2d4708c5a75931a4f67e589d732272565
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-25 18:53:06 +0300

    modified report_common

--- app/models/concerns/report_common.rb
+++ app/models/concerns/report_common.rb
@@ -1,17 +1,17 @@
 module ReportCommon
+  extend ActiveSupport::Concern
+
   METRIC = %w[applied restarted failed failed_restarts skipped pending]
   BIT_NUM = 6
   MAX = (1 << BIT_NUM) -1 # maximum value per metric
 
-  def self.included(base)
-    base.class_eval do
-      # search for a metric - e.g.:
-      # Report.with("failed") --> all reports which have a failed counter > 0
-      # Report.with("failed",20) --> all reports which have a failed counter > 20
-      scope :with, lambda { |*arg| {
-        :conditions => "(#{report_status} >> #{BIT_NUM*METRIC.index(arg[0])} & #{MAX}) > #{arg[1] || 0}"}
-      }
-    end
+  included do
+    # search for a metric - e.g.:
+    # Report.with("failed") --> all reports which have a failed counter > 0
+    # Report.with("failed",20) --> all reports which have a failed counter > 20
+    scope :with, lambda { |*arg| {
+      :conditions => "(#{report_status} >> #{BIT_NUM*METRIC.index(arg[0])} & #{MAX}) > #{arg[1] || 0}"}
+    }
   end
 
   # generate dynamically methods for all metrics
@@ -41,7 +41,7 @@ module ReportCommon
   #when no metric type is specific returns hash with all values
   #passing a METRIC member will return its value
   def status(type = nil)
-    raise(N_("invalid type %s") % type) if type and not METRIC.include?(type)
+    raise "invalid type #{type}" if type and not METRIC.include?(type)
     h = {}
     (type.is_a?(String) ? [type] : METRIC).each do |m|
       h[m] = (read_attribute(self.class.report_status) || 0) >> (BIT_NUM*METRIC.index(m)) & MAX
