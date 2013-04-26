4957e7c7fe64b857cb254201f750b9189a4a37c4
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-26 13:49:34 +0300

    app paths

--- config/application.rb
+++ config/application.rb
@@ -45,10 +45,11 @@ module Foreman
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/lib/sso)
     config.autoload_paths += Dir["#{config.root}/app/models/concerns"]
     config.autoload_paths += Dir["#{config.root}/app/controllers/concerns"]
-    config.autoload_paths += %W(#{config.root}/models/**/*.rb)
-    config.autoload_paths += %W(#{config.root}/controllers/**/*.rb)
+    config.autoload_paths += %W(#{config.root}/app/models/**/*.rb)
+    config.autoload_paths += %W(#{config.root}/app/controllers/**/*.rb)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
