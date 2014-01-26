5ede9e326c1d5f9880eed8a0f9e626d3b5d975ad
Joseph Magen <jmagen@redhat.com>
2014-01-26 17:09:24 +0200

    change smart variable matchers on rename

--- app/models/concerns/nested_ancestry_common.rb
+++ app/models/concerns/nested_ancestry_common.rb
@@ -4,6 +4,7 @@ module NestedAncestryCommon
   included do
     before_save :set_label, :on => [:create, :update, :destroy]
     after_save :set_other_labels, :on => [:update, :destroy]
+    after_save :update_matchers , :on => :update, :if => Proc.new {|obj| obj.label_changed?}
 
     scoped_search :on => :label, :complete_value => :true, :default_order => true
   end
@@ -27,6 +28,15 @@ module NestedAncestryCommon
         end
       end
     end
+  end
+
+  def obj_type
+    self.class.to_s.downcase
+  end
+
+  def update_matchers
+    lookup_values = LookupValue.where(:match => "#{obj_type}=#{label_was}")
+    lookup_values.update_all(:match => "#{obj_type}=#{label}")
   end
 
 end
