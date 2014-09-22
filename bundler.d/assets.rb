04f792b20b6d93cd613b0507cac1632731a9b4d3
Joseph Magen <jmagen@redhat.com>
2014-09-22 12:20:44 +0300

    squash this commit

--- bundler.d/assets.rb
+++ bundler.d/assets.rb
@@ -4,12 +4,12 @@ gem 'execjs'
 gem "jquery-rails"
 gem 'jquery-ui-rails'
 gem "therubyracer", :require => 'v8'
-#gem 'bootstrap-sass'
+gem 'bootstrap-sass'
 #gem "spice-html5-rails"
-#gem "flot-rails"
-#gem "quiet_assets"
+gem "flot-rails"
+gem "quiet_assets"
 #  gem 'gettext_i18n_rails_js', '>= 0.0.8'
 #gem 'gettext',  :require => false
 #gem 'locale'
-#gem "multi-select-rails"
+gem "multi-select-rails"
 #gem 'gridster-rails'
