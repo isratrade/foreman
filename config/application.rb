17a28182c0f0a589ebc27dda256dc91a3c65869b
Joseph Magen <jmagen@redhat.com>
2016-08-09 15:57:39 +0300

    added rack-cors gem + foreman_api_v3 gem

--- config/application.rb
+++ config/application.rb
@@ -154,6 +154,24 @@ module Foreman
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins '*'
+
+        resource '/cors',
+          :headers => :any,
+          :methods => [:post],
+          :credentials => true,
+          :max_age => 0
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :options, :head],
+          :max_age => 0
+      end
+    end
+
+
     # Catching Invalid JSON Parse Errors with Rack Middleware
     config.middleware.insert_before ActionDispatch::ParamsParser, "Middleware::CatchJsonParseErrors"
 
